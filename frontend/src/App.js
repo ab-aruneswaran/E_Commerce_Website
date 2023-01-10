78c3ce074358dd9fb8e9abee638fb8531c5813bd
arun1968333 <aruneswaranab@gmail.com>
2023-01-10 21:21:09 +0530

    Front-End-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Flipcart</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
