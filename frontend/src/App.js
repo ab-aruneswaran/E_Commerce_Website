f22e27fc87a1b94f758ea4dd941e6b38ee35bc6e
arun1968333 <aruneswaranab@gmail.com>
2023-01-10 22:36:22 +0530

    Front-End-React-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-
+import data from './data';
 
 function App() {
   return (
@@ -7,7 +7,25 @@ function App() {
         <a href="/">Flipcart</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                 <p>{product.name}</p>
+              </a>
+            <p>
+                 <strong>${product.price}</strong>
+            </p>
+            <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+        </div>
       </main>
     </div>
   );
