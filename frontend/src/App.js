71d5c91c4b0cd3fb91945a492e536ce597ccd7a6
arun1968333 <aruneswaranab@gmail.com>
2023-01-12 20:11:32 +0530

    Front-End-React-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
-import data from './data';
+import { BrowserRouter, Route , Routes, Link } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+    <div>
       <header>
-        <a href="/">Flipcart</a>
+        <Link to ="/">Flipcart</Link>
       </header>
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
-            <img src={product.image} alt={product.name} />
-            </a>
-            <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-                 <p>{product.name}</p>
-              </a>
-            <p>
-                 <strong>${product.price}</strong>
-            </p>
-            <button>Add to cart</button>
-            </div>
-          </div>
-        ))}
-        </div>
+        <Routes>
+           <Route path="/product/:slug" element={<ProductScreen/>}/>
+           <Route path="/" element={<HomeScreen />} />
+        </Routes>
       </main>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import { useEffect , useState } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+//import data from "../data";
+
+function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await axios.get('/api/products');
+        setProducts(result.data);
+      };
+      fetchData();
+    }, []);
+    return (
+    <div>
+        <h1>Featured Products</h1>
+        <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to ={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                 <p>{product.name}</p>
+              </Link>
+            <p>
+                 <strong>${product.price}</strong>
+            </p>
+            <button>Add to cart</button>
+            </div>
+          </div>
+        ))}
+        </div>
+    </div>);
+}
+export default HomeScreen;
