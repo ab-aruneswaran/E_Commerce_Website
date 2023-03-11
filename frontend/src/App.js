b5d50d902bc148bd271340a361cf7bca045ef5e0
arun1968333 <aruneswaranab@gmail.com>
2023-03-11 19:04:08 +0530

    Front-End-React-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,37 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Badge from 'react-bootstrap/Badge';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './Screens/CartScreen';
 import SigninScreen from './Screens/SigninScreen';
+import SignupScreen from './Screens/SignupScreen';
+import ShippingAddressScreen from './Screens/ShippingAddressScreen';
+import PaymentMethodScreen from './Screens/PaymentMethodScreen';
+import PlaceOrderScreen from './Screens/PlaceOrderScreen';
+import OrderScreen from './Screens/OrderScreen';
 function App() {
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT'});
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
+  }
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
+      <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar  variant="dark">
             <Container>
@@ -31,6 +47,24 @@ function App() {
                     </Badge>
                   )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                  <NavDropdown.Divider />
+                  <Link
+                    className="dropdown-item"
+                    to="#signout"
+                    onClick={signoutHandler}
+                    >SignOut</Link>
+                    </NavDropdown>
+                ):(
+                  <Link className="nav-link" to="/signin">Sign In </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
@@ -41,6 +75,11 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+              <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
+              <Route path="/order/:id" element={<OrderScreen />}></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
--- frontend/src/Screens/SignupScreen.js
+++ frontend/src/Screens/SignupScreen.js
@@ -16,8 +16,10 @@ export default function SigninScreen() {
     const redirectInUrl = new URLSearchParams(search).get('redirect');
     const redirect = redirectInUrl ? redirectInUrl : '/';
 
-    const [email, setEmail] = useState(' ');
-    const [password, setPassword] = useState(' ');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const {userInfo} = state;
@@ -25,10 +27,16 @@ export default function SigninScreen() {
 
     const submitHandler = async (e) => {
         e.preventDefault();
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+            return;
+        }
         try {
-            const { data } = await Axios.post('/api/users/signin', {
+            const { data } = await Axios.post('/api/users/signup', {
+                name,
                 email,
                 password,
+                confirmPassword,
             });
             ctxDispatch({type: 'USER_SIGNIN', payload: data})
             localStorage.setItem('userInfo', JSON.stringify(data));
@@ -46,28 +54,32 @@ export default function SigninScreen() {
     return (
       <Container className="small-container">
         <Helmet>
-            <title>Sign In</title>
+            <title>Sign Up</title>
         </Helmet>
-        <h1 className="my-3">Sign In</h1>
+        <h1 className="my-3">Sign Up</h1>
         <Form onSubmit={submitHandler}>
+            <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control  onChange={(e) => setName(e.target.value)}/>
+            </Form.Group>
             <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type='email' required   onChange={(e) => setEmail(e.target.value)}
-
-                />
+                <Form.Control type='email' required   onChange={(e) => setEmail(e.target.value)}/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type='password' required   onChange={(e) => setPassword(e.target.value)}
-
-                />
+                <Form.Control type='password' required   onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="confirmpassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type='password' required   onChange={(e) => setConfirmPassword(e.target.value)} />
             </Form.Group>
             <div className="mb-3">
-                <Button type="submit">Sign In</Button>
+                <Button type="submit">Sign Up</Button>
             </div>
             <div className="mb-3">
-                New Customer?{' '}
-                <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+                Already have an account?{' '}
+                <Link to={`/signin?redirect=${redirect}`}>Sign-In</Link>
             </div>
         </Form>
       </Container>
