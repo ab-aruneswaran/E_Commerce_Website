1f74dc2a0144d52709139dd324800d9c8bdb5f13
arun1968333 <aruneswaranab@gmail.com>
2023-04-19 21:52:41 +0530

    Front-End-React-App

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,9 @@ app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => {
+app.get('*', (req, res) => 
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-});
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
